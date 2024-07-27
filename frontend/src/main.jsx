9ec55ca3feb4ac8b2821b1912191d3e81723cee9
erictumu1 <erictumu@outlook.com>
2024-07-27 07:21:24 -0400

    Deploy website

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <StoreContextProvider>
+  <StoreContextProvider basename="/TuMunch">
       <App />
   </StoreContextProvider>
     </BrowserRouter>
